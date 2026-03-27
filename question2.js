57450d1e44660f5024598351a95e1b24f85a0e69
BigBen07 <benedictsmartstar@gmail.com>
2026-03-27 12:24:21 +0100

    changing the value of an object property and adding another property to the object

---
+++ question2.js
@@ -0,0 +1,12 @@
+let car = {
+  brand: "Toyota",
+  model: "Corolla",
+  year: 2019,
+  price: 5500000,
+};
+let color = "white";
+car.color;
+car.price = 4800000;
+console.log(car);
+console.log(car.brand);
+console.log(car.price);
